70f638b0dc98995c16c630a47e883d2e760f90b0
Israel Mukisa <mukisaisrael05@gmail.com>
2026-06-30 11:30:16 +0200

    the librarian file

---
+++ Librarian.h
@@ -0,0 +1,21 @@
+#include "Book.h"
+#include "User.h"
+//
+// Created by mukis on 29/06/2026.
+//
+using namespace std;
+#ifndef LIBRARIAN_H
+#define LIBRARIAN_H
+class Librarian {
+private:
+    int LibrarianId;
+    string LibrarianName;
+public:
+    void issueBook( Book& book , User& user );
+    void ReturnBook( Book& book , User& user );
+    Librarian(int Librarian, string LibrarianName ) {
+        this->LibrarianId = Librarian;
+        this->LibrarianName = LibrarianName;
+    };
+};
+#endif //LIBRARIAN_H
